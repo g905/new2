a4a40703ecacc1853631cfd65e51c65f861fa455
g905 <breakestra@yandex.ru>
2022-10-09 21:30:45 +0400

    rewrite everything

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true
+});
